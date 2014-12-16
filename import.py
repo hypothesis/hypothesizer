268c2407bd562e6b218d0cae8ae9d4b353cba245
BigBlueHat <byoung@bigbluehat.com>
2014-12-15 23:02:46 -0500

    Start from most recent annotations

--- import.py
+++ import.py
@@ -3,25 +3,38 @@ import json
 
 import requests
 
+#couch = 'https://hypothesis.cloudant.com/hypothesizer'
+couch = 'http://localhost:5984/hypothesizer'
 user = 'admin'
 password = getpass('Password for CouchDB:')
 
 first_req = requests.get('https://hypothes.is/api/search?limit=1')
 total = first_req.json()['total']
 limit = 200 # h default afaik
+offset = 0
+errors = 0
 
-offset = total - limit # start at the back
-while offset > 0:
+while errors == 0:
     r = requests.get('https://hypothes.is/api/search',
             params = {'limit': limit, 'offset': offset})
     rows = r.json()['rows']
     for row in rows:
         row['_id'] = 'hypothes.is/a/' + row['id']
 
-    r = requests.post('http://localhost:5984/hypothesizer/_bulk_docs',
+    r = requests.post(couch + '/_bulk_docs',
             auth=(user, password),
             headers=({'content-type': 'application/json'}),
             data=json.dumps({'docs': rows}))
+    statuses = r.json()
 
+    for status in statuses:
+        if 'error' in status:
+            errors += 1
+    if errors == 0:
+        print '{0} documents added.'.format(len(statuses) - errors)
+    else:
+        print '{0} documents conflicted.'.format(errors)
+        print '{0} documents added.'.format(len(statuses) - errors)
+        break
     # keep moving
-    offset = offset - limit
+    offset = offset + limit
