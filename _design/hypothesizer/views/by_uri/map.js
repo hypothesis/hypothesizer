66de9df9d363e9878731804407380aa22f310a4b
BigBlueHat <byoung@bigbluehat.com>
2014-12-19 16:54:58 -0500

    Add by_uri; Add to index.html; Add index.html...
    You know...sometimes you just miss somethings...

---
+++ _design/hypothesizer/views/by_uri/map.js
@@ -0,0 +1,7 @@
+function (doc) {
+  if ('document' in doc && 'link' in doc.document) {
+    emit(doc.document.link[0].href, 1);
+  } else if ('uri' in doc) {
+    emit(doc.uri);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -7,15 +7,33 @@ var PouchDB = require('pouchdb');
 var db = new PouchDB(location.protocol + '//' + location.hostname + ':'
     + location.port + '/hypothesizer');
 
+Vue.filter('limit',
+  function (list, n) {
+    return list.slice(0, n);
+  });
+
+Vue.filter('truncate',
+  function (s, n) {
+    return s.slice(0, n) + '...';
+  });
+
 window.Hypothesizer = new Vue({
   el: 'body',
   data: {
     total_public: 0,
-    users: []
+    users: [],
+    uris: []
   },
   computed: {
     total_public_users: function() {
       return this.users.length;
+    },
+    total_public_uris: function() {
+      return this.uris.length;
+    },
+    top_uris: function() {
+      // sort uris by value
+      // return top 100 from the list
     }
   }
 });
@@ -28,4 +46,9 @@ db.query('hypothesizer/by_created')
 db.query('hypothesizer/by_user', {group_level: 1})
   .then(function (res) {
     Hypothesizer.users = res['rows'];
+  });
+
+db.query('hypothesizer/by_uri', {group: true})
+  .then(function (res) {
+    Hypothesizer.uris = res['rows'];
   });
